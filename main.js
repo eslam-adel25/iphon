5446a3279483e31f322fe3e2fbfec8592424e06c
Eslam Adel <eslam.adel2596@gmail.com>
2026-04-27 18:56:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img');
+let contaniner = document.querySelector('.contaniner');
+function phones(phone) {
+    img.src = phone;
+}
+function colors(color) {
+    contaniner.style.background = color;
+}
